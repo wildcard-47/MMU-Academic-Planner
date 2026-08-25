cac61553bbc8472a2d4d64a7861a1bb3466b324f
Mayada <mayadaaljwhari@gmail.com>
2026-08-25 15:10:43 +0800

    Remove the button and change size

--- dashboard.py
+++ dashboard.py
@@ -3,14 +3,8 @@ import customtkinter
 class Dashboard(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("400x150")
+        self.geometry("900x450")
         self.title("MMU Academic Planner")
-
-        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callbck)
-        self.button.pack(padx=20, pady=20)
-
-    def button_callbck(self):
-        print("button clicked")
 
 app = Dashboard()
 app.mainloop()
